ba397903f9990ebdabf81fa8297e02b72e2e73ec
Mikaela Brown <mikaela.brown92@gmail.com>
2018-02-06 21:08:51 -0500

    updated js

--- chatServer.js
+++ chatServer.js
@@ -30,7 +30,7 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function(){// we wait until the client has loaded and contacted us that it is ready to go.
 
-  socket.emit('answer',"Hey, Hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
+  socket.emit('answer',"Hey doll!, I am Kim K bot"); //We start with the introduction;
   setTimeout(timedQuestion, 2500, socket,"What is your Name?"); // Wait a moment and respond with a question.
 
 });
@@ -51,40 +51,38 @@ function bot(data,socket,questionNum) {
 
 /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-  answer= 'Hello ' + input + ' :-)';// output response
+  answer= 'Cool, '+ input+ ', we should be friends!';// output response
   waitTime =2000;
-  question = 'How old are you?';			    	// load next question
+  question = 'You look to be the same age as Kendall? How old are you?';			    	// load next question
   }
   else if (questionNum == 1) {
-  answer= 'Really ' + input + ' Years old? So that means you where born in: ' + (2018-parseInt(input));// output response
+  answer= 'Ah, so a bit older.';// output response
   waitTime =2000;
-  question = 'Where do you live?';			    	// load next question
+  question = 'How did you feel when I broke the Internet?';			    	// load next question
   }
   else if (questionNum == 2) {
-  answer= ' Cool! I have never been to ' + input+'.';
+  answer= ' It was so epic, right?';
   waitTime =2000;
-  question = 'Whats your favorite Color?';			    	// load next question
+  question = 'What hair color should I try next?';			    	// load next question
   }
   else if (questionNum == 3) {
-  answer= 'Ok, ' + input+' it is.';
+  answer= 'Hmm, ' + input+'?, we should preview the color.';
   socket.emit('changeBG',input.toLowerCase());
   waitTime = 2000;
-  question = 'Can you still read the font?';			    	// load next question
+  question = 'Do you like Chicago as a baby name?';			    	// load next question
   }
   else if (questionNum == 4) {
     if(input.toLowerCase()==='yes'|| input===1){
-      answer = 'Perfect!';
+      answer = 'I knew you had good taste!';
       waitTime =2000;
-      question = 'Whats your favorite place?';
+      question = 'So many haters about it. What are your final thoughts on the hair color?';
     }
     else if(input.toLowerCase()==='no'|| input===0){
-        socket.emit('changeFont','white'); /// we really should look up the inverse of what we said befor.
-        answer='How about now?'
-        question='';
-        waitTime =0;
-        questionNum--; // Here we go back in the question number this can end up in a loop
+        answer='What a hater';
+        waitTime =2000;
+        question = 'What are your final thoughts on the hair color?'; // Here we go back in the question number this can end up in a loop
     }else{
-      answer=' I did not understand you. Can you please answer with simply with yes or no.'
+      answer=' Um, this is a yes or no answer.'
       question='';
       questionNum--;
       waitTime =0;
@@ -92,7 +90,7 @@ function bot(data,socket,questionNum) {
   // load next question
   }
   else{
-    answer= 'I have nothing more to say!';// output response
+    answer= 'Thanks for the suggestion! I have to go to the gym now. Bye!';// output response
     waitTime =0;
     question = '';
   }
